c558e8e2e2c352319e120800d07e1453de7c992b
William Jackson <william@subtlecoolness.com>
2019-09-17 13:27:58 -0500

    Clean up configuration and add headers

--- pelican-conf.py
+++ pelican-conf.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
 ARCHIVES_SAVE_AS = ''
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{urlname}.html'
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{urlname}'
@@ -25,16 +21,18 @@ DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU = False
 EXTRA_PATH_METADATA = {
     'extra/googleb417364cc8d4ecab.html_': {'path': 'googleb417364cc8d4ecab.html'},
+    'extra/headers.txt': {'path': '_headers'},
     'extra/robots.txt': {'path': 'robots.txt'}
 }
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_DOMAIN = 'https://accio.subtlecoolness.com'
 GZIP_CACHE = True
 LOAD_CONTENT_CACHE = True
 PATH = 'content'
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 SITENAME = 'Accio Jacksons!'
 SITESUBTITLE = 'An 11-inch holly blog with a phoenix feather core'
-SITEURL = ''
+SITEURL = FEED_DOMAIN
 STATIC_CHECK_IF_MODIFIED = True
 STATIC_PATHS = ['images', 'extra/googleb417364cc8d4ecab.html_', 'extra/robots.txt']
 TAG_SAVE_AS = ''
