2ff4ed8e0d3cf7251f8a7d76175b43e9836511bc
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-02-15 19:17:22 -0600

    Switch from Netlify to GitHub Pages

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,9 +1,3 @@
-import logging
-import os
-
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
-
 ARCHIVES_SAVE_AS = ''
 ARTICLE_EXCLUDES = ['extra']
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{urlname}.html'
@@ -32,12 +26,14 @@ EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'}
 }
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_DOMAIN = 'https://accio.subtlecoolness.com'
 GZIP_CACHE = True
 LOAD_CONTENT_CACHE = True
 PATH = 'content'
 RELATIVE_URLS = False
 SITENAME = 'Accio Jacksons!'
 SITESUBTITLE = 'An 11-inch holly blog with a phoenix feather core'
+SITEURL = FEED_DOMAIN
 SLUGIFY_SOURCE = 'basename'
 STATIC_CHECK_IF_MODIFIED = True
 STATIC_PATHS = ['extra']
@@ -46,20 +42,3 @@ TAGS_SAVE_AS = ''
 THEME = 'themes/accio'
 TIMEZONE = 'America/Chicago'
 TRANSLATION_FEED_ATOM = None
-
-context = os.getenv('CONTEXT')
-url = os.getenv('URL')
-deploy_url = os.getenv('DEPLOY_URL')
-deploy_prime_url = os.getenv('DEPLOY_PRIME_URL')
-
-log.info(f'CONTEXT: {context}')
-log.info(f'URL: {url}')
-log.info(f'DEPLOY_URL: {deploy_url}')
-log.info(f'DEPLOY_PRIME_URL: {deploy_prime_url}')
-
-if context == 'production':
-    FEED_DOMAIN = url
-    SITEURL = url
-else:
-    FEED_DOMAIN = deploy_prime_url
-    SITEURL = deploy_prime_url
