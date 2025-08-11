2899bd79779abcd87d6c19d6f534988c5fdbce5c
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-08-11 16:34:23 -0500

    Clean up pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,44 +1,41 @@
-ARCHIVES_SAVE_AS = ''
-ARTICLE_EXCLUDES = ['extra']
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{urlname}.html'
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{urlname}'
-AUTHOR = 'Rebecca and William Jackson'
+ARCHIVES_SAVE_AS = ""
+ARTICLE_EXCLUDES = ["extra"]
+ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{date:%d}/{urlname}.html"
+ARTICLE_URL = "{date:%Y}/{date:%m}/{date:%d}/{urlname}"
+AUTHOR = "Rebecca and William Jackson"
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-AUTHOR_SAVE_AS = ''
-AUTHORS_SAVE_AS = ''
-CACHE_CONTENT = True
-CACHE_PATH = 'cache'
-CATEGORIES_SAVE_AS = ''
+AUTHOR_SAVE_AS = ""
+AUTHORS_SAVE_AS = ""
+CATEGORIES_SAVE_AS = ""
 CATEGORY_FEED_ATOM = None
-CATEGORY_SAVE_AS = ''
-CHECK_MODIFIED_METHOD = 'md5'
-CONTENT_CACHING_LAYER = 'reader'
+CATEGORY_SAVE_AS = ""
+CHECK_MODIFIED_METHOD = "md5"
+CONTENT_CACHING_LAYER = "reader"
 DEBUG_LAYOUT = False
-DEFAULT_DATE_FORMAT = '%Y-%m-%d'
-DEFAULT_LANG = 'en'
+DEFAULT_DATE_FORMAT = "%Y-%m-%d"
+DEFAULT_LANG = "en"
 DEFAULT_PAGINATION = False
 DELETE_OUTPUT_DIRECTORY = True
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU = False
 EXTRA_PATH_METADATA = {
-    'extra/googleb417364cc8d4ecab.html': {'path': 'googleb417364cc8d4ecab.html'},
-    'extra/robots.txt': {'path': 'robots.txt'}
+    "extra/gitignore.txt": {"path": ".gitignore"},
+    "extra/googleb417364cc8d4ecab.html": {"path": "googleb417364cc8d4ecab.html"},
+    "extra/robots.txt": {"path": "robots.txt"},
 }
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-FEED_DOMAIN = 'https://accio.subtlecoolness.com'
-GZIP_CACHE = True
-LOAD_CONTENT_CACHE = True
-PATH = 'content'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+FEED_DOMAIN = "https://accio.subtlecoolness.com"
+PATH = "content"
 RELATIVE_URLS = False
-SITENAME = 'Accio Jacksons!'
-SITESUBTITLE = 'An 11-inch holly blog with a phoenix feather core'
+SITENAME = "Accio Jacksons!"
+SITESUBTITLE = "An 11-inch holly blog with a phoenix feather core"
 SITEURL = FEED_DOMAIN
-SLUGIFY_SOURCE = 'basename'
+SLUGIFY_SOURCE = "basename"
 STATIC_CHECK_IF_MODIFIED = True
-STATIC_PATHS = ['extra']
-TAG_SAVE_AS = ''
-TAGS_SAVE_AS = ''
-THEME = 'themes/accio'
-TIMEZONE = 'America/Chicago'
+STATIC_PATHS = ["extra"]
+TAG_SAVE_AS = ""
+TAGS_SAVE_AS = ""
+THEME = "themes/accio"
+TIMEZONE = "America/Chicago"
 TRANSLATION_FEED_ATOM = None
